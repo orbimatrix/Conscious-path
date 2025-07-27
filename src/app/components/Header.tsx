3fc62b052b8a5cace6cce14d708260d4f331dc86
Saqib <saqibiqbal27772@gmail.com>
2025-07-27 22:36:37 +0500

    Update Header component: replace menu image with SVG and modify logo to use text SVG

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,10 +14,12 @@ const Header = () => (
     zIndex: 10,
   }}>
     <div style={{ display: "flex", alignItems: "center", gap: 16 }}>
-      <Image src="/window.svg" alt="Menu" width={32} height={32} />
+      <Image src="/img/menu/menu200.svg" alt="Menu" width={32} height={32} />
     </div>
     <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
-      <Image src="/file.svg" alt="Logo" width={32} height={32} />
+      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ display: 'block' }}>
+        <text x="8" y="25" fontSize="28" fontWeight="bold" fill="#B8860B">$</text>
+      </svg>
       <span style={{ fontWeight: 400, fontSize: 28, letterSpacing: 1 }}>
         <span style={{ color: "#E6A14A", fontWeight: 300 }}>SENDA</span>
         <span style={{ color: "#5A5353", fontWeight: 300 }}> CONSCIENTE</span>
