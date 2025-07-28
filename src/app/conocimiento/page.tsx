f715e7ad57b6ea869090e5913a8228dbbeb36bb6
Saqib <saqibiqbal27772@gmail.com>
2025-07-28 17:48:36 +0500

    Refactor styles in globals.css for improved consistency across pages and enhance layout of Registration and NotFound pages for better user experience.

---
+++ src/app/conocimiento/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FooterSection from "../components/FooterSection";
+
+export default function AudioVideoPage() {
+  return (
+    <div>
+      <section className="audio-video-hero">
+        
+      </section>
+      <FooterSection />
+    </div>
+  );
+}
