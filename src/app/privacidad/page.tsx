e9cd70b3d049df9280378bef701c8accaf82fb6c
Saqib <saqibiqbal27772@gmail.com>
2025-07-30 10:39:36 +0500

    Update AvisoLegalPage and MenteUnicaPage content; improve text readability by replacing HTML entities with their character representations for enhanced consistency.

---
+++ src/app/privacidad/page.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import styles from './page.module.css';
+import FooterSection from '../components/FooterSection';
+
+export default function PrivacidadPage() {
+  return (
+    <div className={styles.container}>
+     
+
+      {/* Privacy Information Section */}
+      <div className={styles.privacySection}>
+        <div className={styles.privacyContainer}>
+          <div className={styles.privacyContent}>
+            <p className={styles.infoText}>
+              INFORMACIÓN
+            </p>
+            <h1 className={styles.privacyTitle}>
+              PRIVACIDAD
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      {/* Privacy Content Section */}
+      <div className={styles.contentSection}>
+        <div className={styles.contentContainer}>
+          <div className={styles.prose}>
+            <h2 className={styles.sectionTitle}>
+              PRIVACIDAD Y PROTECCIÓN DE DATOS
+            </h2>
+
+            <h3 className={styles.subsectionTitle}>1 Información general</h3>
+            <p className={styles.paragraph}>
+              A través de la presente política de protección de datos, DigitalMaps OÜ con nº de registro 16551653 (en adelante DigitalMaps OÜ) responsable de la web www.SENDACONSCIENTE.com, en adelante denominada "SENDACONSCIENTE", informa a los USUARIOS que utilicen dicha web acerca de su política de protección de datos personales, con la finalidad de que decidan libre y voluntariamente si desean facilitar a DigitalMaps OÜ los datos personales que les puedan ser solicitados.
+            </p>
+            <p className={styles.paragraph}>
+              En cumplimiento de las leyes de protección de datos y sus reglamentos, le informamos que los datos personales que nos aporte serán objeto de tratamiento en un fichero denominado "USUARIOS SENDACONSCIENTE", responsabilidad de DigitalMaps OÜ con nº de registro 16551653 (en adelante DigitalMaps OÜ) con domicilio en Sepapaja tn 6, Lasnamäe linnaosa, Tallinn 15551, y con correo electrónico de contacto general "info[arroba]SENDACONSCIENTE.com".
+            </p>
+            <p className={styles.paragraph}>
+              Al registrarse en SENDACONSCIENTE, el cliente reconoce haber leídos las presentes condiciones de privacidad de DigitalMaps OÜ (SENDACONSCIENTE) y acepta de forma expresa que las presentes condiciones son parte de la relación contractual.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>1.1 Finalidad del fichero</h4>
+            <p className={styles.paragraph}>
+              Se informa al USUARIO que la finalidad de este fichero que recoge y trata sus datos, será prestar servicio a los USUARIOS de la web SENDACONSCIENTE
+            </p>
+            <p className={styles.paragraph}>
+              De acuerdo con la legislación vigente, el USUARIO tiene el derecho de acceso, rectificación, cancelación y oposición de sus datos. Para ejercitar sus derechos, los USUARIOS pueden dirigirse a DigitalMaps OÜ mediante un correo electrónico a info[arroba]SENDACONSCIENTE.com indicando su NICK y correo electrónico, e indicándonos qué derecho quiere ejercitar de los indicados anteriormente. DigitalMaps OÜ podrá realizar todo tipo de comprobaciones de identidad añadidas para proteger los datos del USUARIO.
+            </p>
+            <p className={styles.paragraph}>
+              Será responsabilidad del USUARIO mantener el total control de la dirección de email que nos proporcione, y usará esa misma cuenta de email para ejercitar sus derechos. Será responsabilidad del USUARIO mantener el total control del teléfono que nos proporcione.
+            </p>
+
+            <h3 className={styles.subsectionTitle}>2 ¿Qué es SENDACONSCIENTE?</h3>
+            <p className={styles.paragraph}>
+              SENDACONSCIENTE es una web destinada al crecimiento espiritual de las personas.
+            </p>
+
+            <h3 className={styles.subsectionTitle}>3 Seguridad</h3>
+            <p className={styles.paragraph}>
+              DigitalMaps OÜ como responsable del fichero en los que figuran los datos del USUARIO, se compromete a respetar la confidencialidad de su información de carácter personal, y para ello todos los datos que nos aporte en el momento de registrarse o iniciar sesión estarán cifrados en SSL (Secure Socket Layer).
+            </p>
+
+            <h4 className={styles.subsectionTitle}>3.1 Alojamiento de datos</h4>
+            <p className={styles.paragraph}>
+              Le informamos que sus datos se encuentran alojados en servidores en la empresa Namecheap (política de privacidad de Namecheap) que es una empresa certificada EU-US para protección de datos personales, y que DigitalMaps OÜ ha tomado todas las medidas técnicas y de seguridad necesarias para que sus datos se guarden en un entorno seguro. Los trabajadores de nuestra empresa así como nuestro encargado del tratamiento han firmado un acuerdo de confidencialidad que asegura la protección de los datos.
+            </p>
+            <p className={styles.paragraph}>
+              Por razones técnicas, de servicio y de protección de datos personales, SENDACONSCIENTE se encuentra alojado en los servidores de protección de datos de máximo nivel. Por la misma razón, la prestación del servicio de correo electrónico se efectúa desde el mismo servidor.
+            </p>
+            <p className={styles.paragraph}>
+              Es responsabilidad del USUARIO utilizar servicios de email y telefonía que garanticen la seguridad y la privacidad de sus datos. DigitalMaps OÜ queda exento de toda responsabilidad en el caso de que el USUARIO utilice servicios de comunicación que no cumplan de forma estricta con las leyes de protección de datos, o que estén ubicadas, física o legalmente, en territorios con leyes permisivas al respecto.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>3.1.1 Bloqueo total o parcial de países</h4>
+            <p className={styles.paragraph}>
+              En base a su posición en determinados países, podemos bloquear total o parcialmente el servicio y/o ciertas secciones de la web. Este bloqueo se basa en el compromiso por cumplir con las diferentes legislaciones nacionales. Dada la gran variación de las leyes a nivel mundial, es posible que no siempre se consiga este propósito. En todo caso, el USUARIO se compromete a limitar o cancelar el uso de este servicio si va en contra de las leyes de su país o territorio. En ningún caso DigitalMaps OÜ será responsable del uso no autorizado de la web por ciudadanos en o de países que no tienen permitido su uso, y el USUARIO exonera a DigitalMaps OÜ de toda responsabilidad legal derivada del uso de esta web, asumiendola el USUARIO como propia.
+            </p>
+
+            <h3 className={styles.subsectionTitle}>4 Sobre sus datos personales</h3>
+            <p className={styles.paragraph}>
+              Cuando Usted se registra en nuestro servicio, nos aportara ciertos datos personales que serán tratados por DigitalMaps OÜ y para lo cual nos da su consentimiento expreso.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>4.1 Datos que le solicitaremos</h4>
+            <p className={styles.paragraph}>
+              Cuando Usted se registra en nuestra web, le pedimos los siguientes datos personales:
+            </p>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>nick</li>
+              <li className={styles.listItem}>email</li>
+              <li className={styles.listItem}>contraseña</li>
+              <li className={styles.listItem}>otras formas de contacto</li>
+              <li className={styles.listItem}>pais</li>
+              <li className={styles.listItem}>IP</li>
+            </ul>
+
+            <h4 className={styles.subsectionTitle}>4.2.1 Finalidad del fichero</h4>
+            <p className={styles.paragraph}>
+              Sus datos personales no serán ni comercializados ni compartidos con terceros.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>4.3 Uso de Cookies</h4>
+            <p className={styles.paragraph}>
+              Nuestra web utiliza cookies (pequeños archivos informáticos que se envían al ordenador de quien accede a páginas web) únicamente para mejorar la experiencia del usuario. Se utilizan cookies de sesión del usuario.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>4.4 Política anti-spam</h4>
+            <p className={styles.paragraph}>
+              DigitalMaps OÜ se declara completamente en contra del envío de comunicaciones comerciales no solicitadas de cualquier tipo de conducta o manifestación conocida como "spam", asimismo se declara comprometido con la lucha contra este tipo de prácticas abusivas. Por tanto, DigitalMaps OÜ garantiza al USUARIO a que bajo ningún concepto los datos personales recogidos en UNAFRUTA serán cedidos, compartidos, transferidos, ni vendidos a ningún tercero, al tiempo que se hará un uso respetuoso y moderado de los mismos.
+            </p>
+            <p className={styles.paragraph}>
+              En el caso de que un USUARIO reciba comunicaciones SENDACONSCIENTE sin haberse registrado, o sin haber dado su consentimiento expreso a dicho registro, puede cancelar dicha suscripción desde los enlaces que se proporcionan en nuestras comunicaciones, en el email info@SENDACONSCIENTE.com.
+            </p>
+
+            <h4 className={styles.subsectionTitle}>4.5 Autorización</h4>
+            <p className={styles.paragraph}>
+              En el momento del registro del USUARIO, DigitalMaps OÜ iniciará un proceso de verificación para asegurar que el USUARIO accede de forma voluntaria y activa a nuestros servicios. Se le enviara un email, al correo electrónico que nos ha proporcionado, con un enlace a toda la información sobre la protección de datos descrita en esta política de privacidad. El USUARIO deberá validad con la acción de acceder al enlace facilitado de verificación en ese email (doble Opt-In) que acepta la presente Política de privacidad. También podrá acceder a través de plataformas en las que se haya verificado previamente, como Google o Facebook.
+            </p>
+            <p className={styles.paragraph}>
+              Ambas acciones quedarán registradas en soportes informáticos. Con este doble proceso Usted nos da su consentimiento expreso, de forma clara e inequívoca, para el tratamiento de sus datos, tal y como se recoge en la política de privacidad de SENDACONSCIENTE.
+            </p>
+            <p className={styles.paragraph}>
+              Si no recibimos contestación por su parte al email enviado en el momento del registro, ejerciendo su derecho de acceso, rectificación, cancelación y oposición, entenderemos que está de acuerdo con el uso y finalidad de la aplicación "SENDACONSCIENTE" y nos da su consentimiento expreso de forma clara e inequívoca y acepta el tratamiento de los datos que nos ha proporcionado.
+            </p>
+            <p className={styles.paragraph}>
+              También es posible que a iniciativa de SENDACONSCIENTE se inicien procesos de verificación de edad.
+            </p>
+            <p className={styles.paragraph}>
+              Del mismo modo Usted nos da su consentimiento expreso de forma clara e inequívoca y acepta el tratamiento de los datos que nos ha proporcionado para el envió de novedades propias, actualizaciones y publicidad.
+            </p>
+            <p className={styles.paragraph}>
+              También nos da su consentimiento expreso de forma clara e inequívoca y acepta que DigitalMaps OÜ podrá usar todos los datos recogidos y aportados por Usted para la resolución de disputas sobre pagos y prestaciones del servicio, en colaboración con las entidades de pago que el USUARIO utilice para adquirir los servicios SENDACONSCIENTE.
+            </p>
+            <p className={styles.paragraph}>
+              El USUARIO por su parte garantiza que los datos personales proporcionados son ciertos, exactos y propios, y se compromete a notificar cualquier cambio o modificación de los mismos. Cualquier pérdida o daño causado al sitio o a la persona responsable del sitio web o a cualquier tercero mediante la comunicación de información errónea, inexacta o incompleta en los formularios de registro será responsabilidad exclusiva del USUARIO.
+            </p>
+            <p className={styles.paragraph}>
+              Con el fin de garantizar la seguridad de las transacciones económicas procedentes de la venta entre el USUARIO y de DigitalMaps OÜ nos reservamos el derecho de solicitar, antes o después de la venta, una prueba documental de la identidad al USUARIO a fin de asegurarse y de asegurar al USUARIO contra una usurpación de identidad y/o de medios de pago.
+            </p>
+            <p className={styles.paragraph}>
+              Puede obtener más información visitando las Condiciones Generales.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer Section */}
+      <FooterSection />
+
+    </div>
+  );
+}
